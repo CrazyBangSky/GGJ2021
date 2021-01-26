a3c667ad88d428402de7a9e4c8d9a1bdbe2dbb85
Liuyi <403360550@qq.com>
2021-01-26 21:43:09 +0800

    init project

---
+++ Source/Balloon/BalloonGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "BalloonGameModeBase.h"
+
---
+++ Source/Balloon/BalloonGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BalloonGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BALLOON_API ABalloonGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
